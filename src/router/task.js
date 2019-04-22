4efcebfde1ba3dfe222266708efd33de726a9ec8
Isao <feat130@hotmail.co.jp>
2019-04-22 08:14:42 -0700

    Add test

--- src/router/task.js
+++ src/router/task.js
@@ -18,17 +18,17 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
-const match = {}
-const sort = {}
+  const match = {}
+  const sort = {}
 
-if (req.query.completed) {
-  match.completed = req.query.completed === 'true'
-}
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
 
-if (req.query.sortBy) {
-  const parts = req.query.sortBy.split(':')
-  sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
-}
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
 
   try {
     await req.user.populate({
